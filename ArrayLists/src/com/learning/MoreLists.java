e91b3e4599ec7d03f87e067c81126b281ee3dcaa
prajyotsogale <psogale212@gmail.com>
2024-08-22 17:50:08 +0530

    Arraylist

---
+++ ArrayLists/src/com/learning/MoreLists.java
@@ -0,0 +1,26 @@
+package com.learning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoreLists {
+    public static void main(String[] args) {
+        String[] items = {"apples","bananas","milk","eggs"};
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+//        System.out.println(list.getClass().getSimpleName());
+//        System.out.println(list.getClass().getName());
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+        System.out.println(groceries);
+
+        ArrayList<String> anotherList = new ArrayList<>(List.of("spices","tea powder","salt"));
+        System.out.println(anotherList);
+
+        groceries.addAll(anotherList);
+        System.out.println(groceries);
+
+    }
+}
