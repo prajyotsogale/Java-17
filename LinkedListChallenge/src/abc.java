3f9c07e13ec3abc98da5b1a97509830fee8b1094
prajyotsogale <psogale212@gmail.com>
2024-09-21 19:52:15 +0530

    more on linkedlist

---
+++ LinkedListChallenge/src/abc.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class abc{
+    private String name;
+    private String artist;
+    ArrayList<String> songs = new ArrayList<>();
+
+    public abc(String name, String artist){
+        this.name = name;
+        this.artist = artist;
+        for (int i = 0; i < song; i++) {
+
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "name='" + name + '\'' +
+                ", artist='" + artist + '\'' +
+                ", songs=" + songs +
+                '}';
+    }
+}
