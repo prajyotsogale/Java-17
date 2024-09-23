f8cbf431e13082f8e629f274a9d24d3873ae2ee3
prajyotsogale <psogale212@gmail.com>
2024-09-23 19:21:42 +0530

    Autoboxing

--- Autoboxing/src/Main.java
+++ Autoboxing/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -15,6 +18,34 @@ public class Main {
         double resultUnboxed = getDoubleObject();
         System.out.println(resultBoxed.getClass().getName());
 
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 22;
+        System.out.println(Arrays.toString(wrapperArray));
+
+        System.out.println(wrapperArray.getClass().getName());
+
+        Character[] characterArray = {'a','b','c','d'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var myList = getList(1,2,3,4,5,6);
+        System.out.println(myList);
+
+    }
+
+    private static int getInt(Integer i){
+        return i;
+    }
+    private static Integer getInteger(int i){
+        return i;
+    }
+
+    private static ArrayList<Integer> getList(int... varargs){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i: varargs){
+            list.add(i);
+        }
+        return list;
     }
 
     private static Double getDoubleObject(){
